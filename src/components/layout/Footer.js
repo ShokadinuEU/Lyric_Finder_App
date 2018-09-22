2f4f6dd094ae95c0811c132c72b51e5b21a0b14c
Timotei Dumitru <dumitru.timy@gmail.com>
2018-09-22 13:56:27 +0100

    finish to style lyric page and make footer well placed

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 class Footer extends Component {
   render() {
     return (
-      <div class="footer-copyright text-center py-2">© 2018 Copyright:
+      <div class="footer footer-copyright text-center">© 2018 Copyright:
       <a href="https://github.com/ShokadinuEU"> ShokadinuEU</a>
     </div>
     )
